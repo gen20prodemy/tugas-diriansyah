2abfe6342cfc6070b2fbce59cfc573be8523e8dd
Diri03 <161284781+Diri03@users.noreply.github.com>
2024-03-05 10:48:32 +0700

    Update ThrowAble.java

--- gen-gen20-github-java-exception/src/tugas6/ThrowAble.java
+++ gen-gen20-github-java-exception/src/tugas6/ThrowAble.java
@@ -5,14 +5,10 @@ import java.util.Scanner;
 public class ThrowAble {
     public static void main(String[] args) throws Throwable {
         Scanner userInput = new Scanner(System.in);
-        try {
-            int[] arr = {1, 2, 3};
-            System.out.print("Index = ");
-            int indeks = userInput.nextInt();
-            System.out.println("Nilai array pada index ke-" + indeks + " = " + arr[indeks]);    
-        } catch (Throwable e) {
-            System.out.println("Error: " + e.getMessage());
-        }
+        int[] arr = {1, 2, 3};
+        System.out.print("Index = ");
+        int indeks = userInput.nextInt();
+        System.out.println("Nilai array pada index ke-" + indeks + " = " + arr[indeks]);    
         userInput.close();
     }
 }

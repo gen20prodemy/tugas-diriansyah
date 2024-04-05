5a7ca517e56128e24c676d089737352de72e8400
Diri03 <diriansyah15@gmail.com>
2024-04-05 16:19:05 +0700

    add tugas jwt security

---
+++ gen-gen20-java-springboot-pos-security/src/main/java/com/prodemy/springbootjpa/request/AuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.prodemy.springbootjpa.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationRequest {
+    private String username;
+    String password;
+}

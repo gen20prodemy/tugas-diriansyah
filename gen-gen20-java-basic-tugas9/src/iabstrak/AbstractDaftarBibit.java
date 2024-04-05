4e530dd0f4d5164c4703463fd12dbb29c6d6601f
Diri03 <diriansyah15@gmail.com>
2024-04-05 18:54:41 +0700

    rename repos oop2

--- gen-gen20-java-basic-tugas9/src/iabstrak/AbstractDaftarBibit.java
+++
@@ -1,8 +0,0 @@
-package iabstrak;
-
-public abstract class AbstractDaftarBibit {
-    public abstract void tambahListBibit(String nama, Integer harga);
-    public abstract void hapusListBibit(String nama);
-    public abstract void ubahListBibit(String nama, Integer harga);
-    public abstract void tampilkanListBibit();
-}
--- gen-gen20-java-basic-tugas9/src/iabstrak/IDaftarMakanan.java
+++
@@ -1,8 +0,0 @@
-package iabstrak;
-
-public interface IDaftarMakanan{
-    public void tambahListMakanan(String name, Integer price);
-    public void hapusListMakanan(String name);
-    public void tampilkanListMakanan();
-    public void ubahListMakanan(String name, Integer price);
-}
